eadf6b9dfe9c84434637c9fb2608c893d1aaa8d0
Ko Kyung Ho <162261412+yoaruku@users.noreply.github.com>
2024-03-15 00:39:48 -0700

    [Bronze V] Title: 윤년, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+
+        if( (num1 % 4 == 0) && ( (num1 % 100 != 0) || (num1 % 400 == 0))){
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+    }
+}
