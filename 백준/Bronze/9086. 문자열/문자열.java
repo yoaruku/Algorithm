642edaa8713679a59b00ecddb5fadbcdfe52fe56
Ko Kyung Ho <162261412+yoaruku@users.noreply.github.com>
2024-03-18 00:22:12 -0700

    [Bronze V] Title: 문자열, Time: 228 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int index = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < index; i++) {
+
+            String str = sc.nextLine();
+            System.out.println("" + str.charAt(0) + str.charAt(str.length() - 1));
+        }
+    }
+}
