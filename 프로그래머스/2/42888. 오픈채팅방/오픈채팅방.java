0735ca8d0e4ee619b0151e14498e460870e240b3
Ko Kyung Ho <rhrudgh12@naver.com>
2024-11-14 19:41:57 +0900

    [level 2] Title: 오픈채팅방, Time: 155.30 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,20 +2,27 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
+        
+        // 닉네임 저장
         Map<String, String> name = new HashMap<>();
 
         for (String str : record){
             String[] split = str.split(" ");
 
+            // 변경일 때만 닉네임 저장
             if (split.length == 3){
                 name.put(split[1], split[2]);
             }
         }
 
+        // 결과 String 저장
         List<String> result = new ArrayList<>();
 
         for (String str : record){
+            
             String[] split = str.split(" ");
+            
+            // Enter, Leave만 String 생성
             switch (split[0]){
                 case "Enter" : result.add(name.get(split[1]) + "님이 들어왔습니다."); break;
                 case "Leave": result.add(name.get(split[1]) + "님이 나갔습니다."); break;
@@ -23,6 +30,7 @@ class Solution {
             }
         }
 
+        // 결과 List -> String[] 형식으로 변경
         return result.toArray(new String[result.size()]);
     }
 }
