e7f26eb612972f5eb621308c46a44db5959339b3
Ko Kyung Ho <rhrudgh12@naver.com>
2025-02-02 13:31:58 +0900

    [Silver IV] Title: 덱, Time: 292 ms, Memory: 20040 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,163 +1,108 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
-/**
- * 정수를 저장하는 덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
- * <p>
- * 명령은 총 여덟 가지이다.
- * <p>
- * push_front X: 정수 X를 덱의 앞에 넣는다.
- * push_back X: 정수 X를 덱의 뒤에 넣는다.
- * pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * size: 덱에 들어있는 정수의 개수를 출력한다.
- * empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
- * front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- * back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
- */
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    
+    static Deque<Integer> deque = new ArrayDeque<>();
 
-    public class CustomDeque {
-        List<Integer> numbers;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        public CustomDeque() {
-            numbers = new ArrayList<>();
-        }
+        int N = Integer.parseInt(br.readLine());
 
-        public void push_front(int number) {
-            numbers.add(number);
-        }
+        for (int i = 0; i < N; i++) {
+            String commandLine = br.readLine();
+            String[] lines = commandLine.split(" ");
 
-        public void push_back(int number) {
-            numbers.add(0, number);
-        }
+            switch (lines[0]) {
+                case "push_front":
+                    int xFront = Integer.parseInt(lines[1]);
+                    pushFront(xFront);
+                    break;
 
-        public void pop_front() {
-            System.out.println(numbers.get(0));
-            numbers.remove(0);
-        }
+                case "push_back":
+                    int xBack = Integer.parseInt(lines[1]);
+                    pushBack(xBack);
+                    break;
 
-        public void pop_back() {
-            System.out.println(numbers.get(numbers.size() - 1));
-            numbers.remove(numbers.size() - 1);
-        }
+                case "pop_front":
+                    popFront();
+                    break;
 
-        public void size() {
-            System.out.println(numbers.size());
-        }
+                case "pop_back":
+                    popBack();
+                    break;
 
-        public void empty() {
-            System.out.println(numbers.isEmpty() ? 1 : 0);
-        }
+                case "size":
+                    size();
+                    break;
 
-        public void front() {
-            System.out.println(numbers.get(0));
-        }
+                case "empty":
+                    empty();
+                    break;
 
-        public void back() {
-            System.out.println(numbers.get(numbers.size() - 1));
+                case "front":
+                    front();
+                    break;
+
+                case "back":
+                    back();
+                    break;
+            }
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void pushFront(int x) {
+        deque.addFirst(x);
+    }
 
-        class CustomDeque {
-            List<Integer> numbers;
+    public static void pushBack(int x) {
+        deque.addLast(x);
+    }
 
-            public CustomDeque() {
-                numbers = new ArrayList<>();
-            }
-
-            public void push_front(int number) {
-                numbers.add(0,number);
-            }
-
-            public void push_back(int number) {
-                numbers.add(number);
-            }
-
-            public void pop_front() {
-                if (numbers.isEmpty()) {
-                    System.out.println(-1);
-                    return;
-                }
-                System.out.println(numbers.get(0));
-                numbers.remove(0);
-            }
-
-            public void pop_back() {
-                if (numbers.isEmpty()) {
-                    System.out.println(-1);
-                    return;
-                }
-                System.out.println(numbers.get(numbers.size() - 1));
-                numbers.remove(numbers.size() - 1);
-            }
-
-            public void size() {
-                System.out.println(numbers.size());
-            }
-
-            public void empty() {
-                System.out.println(numbers.isEmpty() ? 1 : 0);
-            }
-
-            public void front() {
-                if (numbers.isEmpty()) {
-                    System.out.println(-1);
-                    return;
-                }
-                System.out.println(numbers.get(0));
-            }
-
-            public void back() {
-                if (numbers.isEmpty()) {
-                    System.out.println(-1);
-                    return;
-                }
-                System.out.println(numbers.get(numbers.size() - 1));
-            }
+    public static void popFront() {
+        if (deque.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(deque.pollFirst());
         }
+    }
 
-        CustomDeque deque = new CustomDeque();
+    public static void popBack() {
+        if (deque.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(deque.pollLast());
+        }
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void size() {
+        System.out.println(deque.size());
+    }
 
-        int roof = Integer.parseInt(br.readLine());
+    public static void empty() {
+        if (deque.isEmpty()) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
 
-        for (int i = 0; i < roof; i++) {
-            String[] split = br.readLine().split(" ");
+    public static void front() {
+        if (deque.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(deque.peekFirst());
+        }
+    }
 
-            switch (split[0]) {
-                case "push_front":
-                    deque.push_front(Integer.parseInt(split[1]));
-                    break;
-                case "push_back":
-                    deque.push_back(Integer.parseInt(split[1]));
-                    break;
-                case "pop_front":
-                    deque.pop_front();
-                    break;
-                case "pop_back":
-                    deque.pop_back();
-                    break;
-                case "size":
-                    deque.size();
-                    break;
-                case "empty":
-                    deque.empty();
-                    break;
-                case "front":
-                    deque.front();
-                    break;
-                case "back":
-                    deque.back();
-                    break;
-            }
+    public static void back() {
+        if (deque.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(deque.peekLast());
         }
     }
 }
