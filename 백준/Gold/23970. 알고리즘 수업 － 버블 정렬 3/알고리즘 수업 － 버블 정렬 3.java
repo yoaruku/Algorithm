db184de6880c42e04a97b912ac52eccd47040c56
Ko Kyung Ho <rhrudgh12@naver.com>
2025-02-24 06:27:48 +0900

    [Gold IV] Title: 알고리즘 수업 - 버블 정렬 3, Time: 428 ms, Memory: 19460 KB -BaekjoonHub

--- 백준/Gold/23970. 알고리즘 수업 － 버블 정렬 3/알고리즘 수업 － 버블 정렬 3.java
+++ 백준/Gold/23970. 알고리즘 수업 － 버블 정렬 3/알고리즘 수업 － 버블 정렬 3.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,20 +11,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        List<Integer> a = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
+        int[] a = new int[n];
+        int[] b = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a.add(Integer.parseInt(st.nextToken()));
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            b.add(Integer.parseInt(st.nextToken()));
+            b[i] = Integer.parseInt(st.nextToken());
         }
-
-        if ( a.equals(b) ){
+        
+        if( Arrays.equals(a, b)){
             System.out.println(1);
             return;
         }
@@ -35,14 +34,14 @@ public class Main {
 
             second:
             for (int j = 0; j < n-1-i; j++) {
-                if (a.get(j) > a.get(j+1)) {
-                    int temp = a.get(j);
-                    a.set(j, a.get(j+1));
-                    a.set(j+1, temp);
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                     swaped = true;
 
-                    if(a.get(j).equals(b.get(j))) {
-                        if( a.equals(b) ) {
+                    if( a[j] == b[j] ) {
+                        if (Arrays.equals(a, b)) {
                             System.out.println(1);
                             return;
                         }
