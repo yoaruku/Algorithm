ecf22bfcc299571854c09d378622a94ef68ee6d4
Ko Kyung Ho <rhrudgh12@naver.com>
2025-03-16 03:46:01 +0900

    [Bronze I] Title: XORXORXOR, Time: 136 ms, Memory: 14404 KB -BaekjoonHub

---
+++ Java/백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(input[2]);
+
+        for (int i = 0 ; i < c; i++){
+            a ^= b;
+        }
+
+        System.out.println(a);
+    }
+}
