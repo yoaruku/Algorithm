f2f4bca5bf78a96a0ab153d07897df8b3245266f
Ko Kyung Ho <rhrudgh12@naver.com>
2025-04-25 19:13:47 +0900

    [level 1] Title: 예산, Time: 0.52 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int sum = 0;
+        int i;
+        
+        for(i = 0 ; i < d.length; i++){
+            sum += d[i];
+            
+            if(sum > budget){
+                break;
+            }
+        }
+        
+        return i;
+    }
+}
