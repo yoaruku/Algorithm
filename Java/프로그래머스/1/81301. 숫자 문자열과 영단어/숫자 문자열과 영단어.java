6bbc845c8bb4628cac16c75b3d573b6f3d813143
Ko Kyung Ho <rhrudgh12@naver.com>
2025-05-03 16:17:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.37 ms, Memory: 83.7 MB -BaekjoonHub

--- Java/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ Java/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,51 +1,13 @@
 class Solution {
     public int solution(String s) {
         
+        String[] words = {"zero", "one", "two", "three", "four", 
+                          "five", "six", "seven", "eight", "nine"};
         
-        StringBuilder sb = new StringBuilder();
-        StringBuilder result = new StringBuilder();
-        
-        for(char c : s.toCharArray()){
-            
-            if(Character.isAlphabetic(c)){
-                sb.append(c);
-            }
-            if(sb.toString().equals("zero")){
-                result.append(0);sb.setLength(0);
-            }
-            if(sb.toString().equals("one")){
-                result.append(1);sb.setLength(0);
-            }
-            if(sb.toString().equals("two")){
-                result.append(2);sb.setLength(0);
-            }
-            if(sb.toString().equals("three")){
-                result.append(3);sb.setLength(0);
-            }
-            if(sb.toString().equals("four")){
-                result.append(4);sb.setLength(0);
-            }
-            if(sb.toString().equals("five")){
-                result.append(5);sb.setLength(0);
-            }
-            if(sb.toString().equals("six")){
-                result.append(6);sb.setLength(0);
-            }
-            if(sb.toString().equals("seven")){
-                result.append(7);sb.setLength(0);
-            }
-            if(sb.toString().equals("eight")){
-                result.append(8);sb.setLength(0);
-            }
-            if(sb.toString().equals("nine")){
-                result.append(9);sb.setLength(0);
-            }
-
-            if(Character.isDigit(c)){
-                result.append(c);
-            }
+        for (int i = 0; i < words.length; i++) {
+            s = s.replaceAll(words[i], String.valueOf(i));
         }
         
-        return Integer.valueOf(String.valueOf(result));
+        return Integer.parseInt(s);
     }
 }
