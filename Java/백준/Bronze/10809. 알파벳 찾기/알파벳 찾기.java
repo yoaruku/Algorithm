cd5d4de039196a9bae928ed0e553495bafd98a48
Ko Kyung Ho <rhrudgh12@naver.com>
2025-05-25 03:15:17 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 136 ms, Memory: 16224 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Character> input = br.readLine()
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+
+        for (char c = 'a'; c <= 'z'; c++){
+            System.out.print(input.indexOf(c)+" ");
+        }
+    }
+}
